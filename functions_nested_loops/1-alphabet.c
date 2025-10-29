39cfcbde661d8759114f7c180635daab34df5a5b
Nano420x <jdjjsudjdjdhs@gmail.com>
2025-10-29 13:45:38 +0000

    add 1 script for holberton exercise

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+void print_alphabet(void)
+{
+	char print_alphabet = ('a');
+		while (print_alphabet <= 'z')
+		{
+		_putchar (print_alphabet);
+		print_alphabet++;
+		}
+		_putchar('\n');
+		}
+
+
+
+
+
+
+
