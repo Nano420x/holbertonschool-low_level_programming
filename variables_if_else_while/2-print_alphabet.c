4e2ed75fc4a1ff09e8e3f75ff3c4ecc4d4474ac2
Nano420x <jdjjsudjdjdhs@gmail.com>
2025-10-29 08:57:54 +0000

    fix the code with betty style

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alphabet = 'a';
-		while(alphabet <= 'z')
-		{
+
+	while (alphabet <= 'z')
+	{
 		putchar(alphabet);
 		alphabet++;
-		}
-		putchar('\n');
+	}
+	putchar('\n');
+
 	return (0);
-		}
+}
+
