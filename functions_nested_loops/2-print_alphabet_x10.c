4e7f6a93c1b4c42f3cf8fd0d86e45f14bdebea81
Nano420x <jdjjsudjdjdhs@gmail.com>
2025-10-29 14:03:33 +0000

    fix the script with betty style

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,19 @@
-#include"main.h"
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ */
 void print_alphabet_x10(void)
 {
-    char print_alphabet_x10;
-    char print;
-		for(print_alphabet_x10='0'; print_alphabet_x10 <= '9'; print_alphabet_x10++) {
-            for(print = 'a'; print <= 'z'; print++) {
-                _putchar(print);
-            }
-            
-         _putchar('\n');   	
-		}
+    char letter;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+            _putchar(letter);
+        }
+        _putchar('\n');
+    }
 }
