0c795016ac351eb3bc145bc8b7e7e2c4814b79bd
Nano420x <jdjjsudjdjdhs@gmail.com>
2025-10-31 08:43:33 +0000

    add 1script for holberton exercise

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isdigit- il fais des trucs plutot pas mal
+* @c: int le c pour qu'il vaux un truc
+* Return: Always 0 (Success)
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+return (0);
+}
