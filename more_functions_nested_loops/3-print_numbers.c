cfdc15f605b7ff7cf23054b85f6aa1bf95af9fe8
Nano420x <jdjjsudjdjdhs@gmail.com>
2025-10-31 09:17:34 +0000

    add 3 script and main for holberton exercise

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers- Starfoula
+* Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
