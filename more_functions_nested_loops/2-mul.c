29e07d516391b840549e38a0b12c05e95f73a099
Nano420x <jdjjsudjdjdhs@gmail.com>
2025-10-31 08:45:09 +0000

    add 2 script and main for holberton exercise

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul- multiplication des deux valeurs
+* @a: premiere valeur
+* @b: deuxieme valeur
+ * Return: la valeur de la multi des deux valeurs
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
