6a89eb17e7825125af303f69bc3d799b52ed8f71
Nano420x <jdjjsudjdjdhs@gmail.com>
2025-11-02 17:01:50 +0000

    project 3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,17 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -25,5 +14,4 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
-
-#endif /* MAIN_H */
+#endif
