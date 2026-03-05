c7816bd5e67465c9e05b5ca545dc8d3480a386a9
Nano420x <jdjjsudjdjdhs@gmail.com>
2026-03-05 11:47:28 +0100

    task 0

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int choice = 0;
+
+    printf("Simple Calculator\n");
+
+    printf("1) Add\n");
+    printf("2) Substract\n");
+    printf("3) Multiply\n");
+    printf("4) Divide\n");
+    printf("0) Quit\n");
+
+    scanf("%d", &choice);
+
+    printf("Choice: %d\n", choice);
+
+    if (choice == 0)
+        printf("Bye!\n");
+
+    return (0);
+}
