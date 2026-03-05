28d7cc4d981d878a8b2bd8a9c1d80ef0ebab70e3
Nano420x <jdjjsudjdjdhs@gmail.com>
2026-03-05 12:10:04 +0100

    task 1

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
     int choice = 0;
 
     printf("Simple Calculator\n");
-
     printf("1) Add\n");
     printf("2) Substract\n");
     printf("3) Multiply\n");
     printf("4) Divide\n");
     printf("0) Quit\n");
 
+    printf("Enter Choice: ");
     scanf("%d", &choice);
 
     printf("Choice: %d\n", choice);
 
-    if (choice == 0)
+    if (choice > 4 || choice < 0)
+    {
+        printf("Invalid Choice\n");
+    }
+    else if (choice == 0)
+    {
         printf("Bye!\n");
+        return (0); 
+    }
+    else 
+    {
+        printf("You selected option %d\n", choice);
+    }
 
     return (0);
 }
