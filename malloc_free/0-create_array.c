ba2ee2a12f9a1825d19b9c0ab72f7f8f2271c26e
Nano420x <jdjjsudjdjdhs@gmail.com>
2026-03-10 17:01:03 +0100

    fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-char *create_array(unsigned int size, char c);
+/*
+ * betty
+*/
+char *create_array(unsigned int size, char c)
 {
     char *tab;
     unsigned int i = 0;
