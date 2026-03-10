6dc1583b0357b7177f00a9bccf2b02f9f79840a6
Nano420x <jdjjsudjdjdhs@gmail.com>
2026-03-10 16:57:45 +0100

    send task 1

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
+{
+    char *tab;
+    unsigned int i = 0;
+
+    if (size == 0)
+    {
+        return (NULL);
+    }
+
+    tab = malloc(sizeof(char) * size);
+
+    if (tab == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        tab[i] = c;
+    }
+
+    return tab;
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+malloc project filler texttttttttttttttttttttttttttttttt
