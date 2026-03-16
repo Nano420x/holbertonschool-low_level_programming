0a750cdd5db09fe113b9b9a4b3bd233db72a86f5
Nano420x <jdjjsudjdjdhs@gmail.com>
2026-03-16 14:04:20 +0100

    add task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+    char *new;
+    unsigned int len = 0, index = 0;
+
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+
+    new = malloc(sizeof(char) * (len + 1));
+
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+
+    while (index <= len)
+    {
+        new[index] = str[index];
+
+        index++;
+    }
+    
+    new[index] = '\0';
+
+    return (new);
+}
